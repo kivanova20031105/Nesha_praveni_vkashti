ead907bbf3818e61db65ab1c3db5cf967ecc53d1
kivanova20031105 <43601190+kivanova20031105@users.noreply.github.com>
2018-10-09 21:25:28 +0300

    Add files
    Add files

---
+++ dialog.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Kak se kazvash?";
+    char ime[30];
+    cin>>ime;
+    cout<<"Zdravey!"<<ime<<"!"<<"Az se kazvam Ivan";
+    char ime[30];
+    cin>>ime;
+    cout<<"Priqtno mi e da se zapoznaem"<<<<"!";
+    cout>>"Kade jivesh?";
+    char ime[30];
+    cin>>ime
+    cout<<"Ne sam hodil tam!"<<endl;
+    return 0;
+}
