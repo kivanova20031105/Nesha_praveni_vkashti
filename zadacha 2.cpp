ba7b1b167b3c3f373e19fb1abcfc53918890adc3
kivanova20031105 <43601190+kivanova20031105@users.noreply.github.com>
2018-10-09 21:27:13 +0300

    Add files
    Add files

---
+++ zadacha 2.cpp
@@ -0,0 +1,13 @@
+/*Napravih programa
+koqto presmqta
+lice na kvadrat*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    int lice=a*a;
+    cout<<lice;
+    return 0;
+}
