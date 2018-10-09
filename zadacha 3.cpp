619047380146463d7db905a564b147ef9fae3856
kivanova20031105 <43601190+kivanova20031105@users.noreply.github.com>
2018-10-09 21:27:38 +0300

    Add files

---
+++ zadacha 3.cpp
@@ -0,0 +1,13 @@
+/*Napisah ptograma
+koqto smqta lice
+na trapec*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    double b1,b2,h,S;
+    cin>>b1>>b2>>h;
+    S=((b1+b2)*h)/2;
+    cout<<"S="<<S;
+    return 0;
+}
