38bc1796da0ca9d988879e4af6442d90b0574476
kivanova20031105 <43601190+kivanova20031105@users.noreply.github.com>
2018-10-09 21:26:18 +0300

    Add files
    Add fiels

---
+++ proba.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Kak se kazvash?";
+    char ime [30];
+    cin>>ime;
+    cout<<"Zdravey!";
+    cout<<"A ti kak se kazvash?";
+    char ime [30];
+    cin>>ime;
+    cout<<"Na kolko si godini?"
+    char godini [2];
+    cin>>godini;
+    cout<<"A ti na kolko si godini?";
+    char godini [2]<<endl;
+    return 0;
+}
